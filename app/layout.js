7a606f49589fadcc5606143f92fe3f9d36696b67
Adi1506A <154859495+Adi1506A@users.noreply.github.com>
2026-03-25 17:38:50 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,18 @@
+import "./globals.css";
+import Link from "next/link";
 
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <nav className="p-4 bg-gray-200 flex gap-4">
+          <Link href="/">Dashboard</Link>
+          <Link href="/projects">Add Project</Link>
+          <Link href="/pipeline">Pipeline</Link>
+          <Link href="/login">Login</Link>
+        </nav>
+        {children}
+      </body>
+    </html>
+  );
+}
