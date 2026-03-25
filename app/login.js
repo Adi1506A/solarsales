de4dbc95683b4a54805a31ed3b394bf9a0bb7acc
Adi1506A <154859495+Adi1506A@users.noreply.github.com>
2026-03-25 17:37:07 +0530

    Update login.js

--- app/login.js
+++ app/login.js
@@ -1,1 +1,27 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+
+  const login = () => {
+    localStorage.setItem("user", email);
+    router.push("/");
+  };
+
+  return (
+    <div className="p-10">
+      <h1>Login</h1>
+      <input
+        className="border p-2"
+        placeholder="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={login} className="ml-2">
+        Login
+      </button>
+    </div>
+  );
+}
